2b4b182a526741e4021d17fc6af4290db045d7d5
Konrad Lykowski <konrad.lykowski@outlook.com>
2017-08-25 18:52:02 +0200

    Update LowestCommonAncestor.java

--- binary_search_tree_lowest_common_ancestor/LowestCommonAncestor.java
+++ binary_search_tree_lowest_common_ancestor/LowestCommonAncestor.java
@@ -15,22 +15,6 @@ public class LowestCommonAncestor {
         Node right;
     }
 
-    public static void main(String[] args) {
-        Node a3 = new Node(3, null, null);
-        Node a2 = new Node(2, null, null);
-
-        Node a1 = new Node(1, a3, a2);
-        Node a6 = new Node(6, null, null);
-
-        Node a9 = new Node(9, null, null);
-        Node a4 = new Node(4, a1, a6);
-
-        Node a8 = new Node(8, a4, a9);
-
-        System.out.println(lca(a8, 3, 2).data);
-    }
-
-
     static Node lca(Node n, int v1, int v2) {
         while(n!=null)
         {
